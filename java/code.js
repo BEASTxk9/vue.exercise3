0af38a9f182844145af156764007b1243dc827c4
shane <shanestevensxk9@gmail.com>
2022-07-05 11:13:50 +0200

    update

--- java/code.js
+++ java/code.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     template: `
     <div class="text-center" id="pop">
-    <label class="py-2">Please enter any key</label><br>
+    <label class="py-2"><b>Please enter any key</b></label><br>
         <input class="text-center py-2 w-25" id="input" type="name" required v-model="key" maxlength="1" placeholder="Enter a key"><br>
         <button v-on:click="getData()">Click</button>
         <p class="py-2">You have pressed <b>"<span id="output"></span>"</b>.</p>
